d804da94f92a976b7f1bde61f6c7d9b6a8b76537
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-09 14:14:27 -0300

    Create FakeScheduleRepository to tests

---
+++ backend/src/modules/schedules/repositories/fakes/FakeScheduleRepository.ts
@@ -0,0 +1,58 @@
+import { uuid } from 'uuidv4';
+import { isEqual, getDate, getMonth, getYear } from 'date-fns';
+import IScheduleRepository from '@modules/schedules/repositories/IScheduleRepository';
+import ICreateScheduleDTO from '@modules/schedules/dtos/ICreateScheduleDTO';
+import IFindALlInDayDTO from '@modules/schedules/dtos/IFindAllInDayDTO';
+import IFindAllInMonthDTO from '@modules/schedules/dtos/IFindAllInMonthDTO';
+import Schedule from '@modules/schedules/typeorm/entities/Schedule';
+
+class FakeScheduleRepository implements IScheduleRepository {
+  private schedules: Schedule[] = [];
+
+  public async create(data: ICreateScheduleDTO): Promise<Schedule> {
+    const schedule = new Schedule();
+    Object.assign(schedule, { id: uuid, data });
+    this.schedules.push(schedule);
+    return schedule;
+  }
+
+  public async update(schedule: Schedule): Promise<Schedule> {
+    const findIndex = this.schedules.findIndex(
+      checkSchedule => checkSchedule.id === schedule.id,
+    );
+    this.schedules[findIndex] = schedule;
+    return schedule;
+  }
+
+  public async findByDate(date: Date): Promise<Schedule | undefined> {
+    return this.schedules.find(schedule => isEqual(schedule.start, date));
+  }
+
+  public async findAllInDay({
+    day,
+    month,
+    year,
+  }: IFindALlInDayDTO): Promise<Schedule[]> {
+    return this.schedules.filter(schedule => {
+      return (
+        getDate(schedule.start) === day &&
+        getMonth(schedule.start) + 1 === month &&
+        getYear(schedule.start) === year
+      );
+    });
+  }
+
+  public async findAllInMonth({
+    month,
+    year,
+  }: IFindAllInMonthDTO): Promise<Schedule[]> {
+    return this.schedules.filter(schedule => {
+      return (
+        getMonth(schedule.start) + 1 === month &&
+        getYear(schedule.start) === year
+      );
+    });
+  }
+}
+
+export default FakeScheduleRepository;
