e26e18523357a6c76ba2af05fe7ca644c8dabe0c
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-09 14:30:35 -0300

    Config dependency injection

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -1,1 +1,9 @@
 import { container } from 'tsyringe';
+
+import IScheduleRepository from '@modules/schedules/repositories/IScheduleRepository';
+import ScheduleRepository from '@modules/schedules/typeorm/repositories/ScheduleRepository';
+
+container.registerSingleton<IScheduleRepository>(
+  'ScheduleRepository',
+  ScheduleRepository,
+);
