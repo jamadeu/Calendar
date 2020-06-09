8b21574e20419f3975af5f0c43ee975652d05e7d
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-09 10:41:57 -0300

    Migration shedules

---
+++ backend/src/shared/typeorm/migrations/1591707685180-CreateSchedules.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateSchedules1591707685180
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'schedules',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'start',
+            type: 'timestamp with time zone',
+          },
+          {
+            name: 'end',
+            type: 'timestamp with time zone',
+          },
+          {
+            name: 'info',
+            type: 'timestamp with time zone',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('schedules');
+  }
+}
